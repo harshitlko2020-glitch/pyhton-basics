4481eec1664847916f11cb5611668895195d5778
harshitlko2020-glitch <harshit.lko2020@gmail.com>
2025-11-05 19:58:57 +0530

    dashboard.st

---
+++ dashboard/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Titanic Data Dashboard')
+
+st.markdown("Welcome to The Titanic Dashboard! Use the sidebar to filter the data and explore various visuals")
---
+++ dashboard/pages/dashboard.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd 
+import seaborn as sns
+import plotly.express as px
+
+df = sns.load_dataset("titanic")
+filtered_df = df.copy()
+
+
+sidebar = st.sidebar
+
+sidebar.title("Dashboard Filters")
+gender = sidebar.multiselect("Gender",options=df["sex"].unique())
+
+pclass = sidebar.multiselect("Passengers Class",options=df['class'].unique())
+
+age = sidebar.slider("age range",min_value=int(df['age'].min()),
+                     max_value=int(df["age"].max()),
+                     value=(int(df['age'].min()),int(df['age'].max())))
+
+if gender:
+    filtered_df = filtered_df[filtered_df["sex"].isin(gender)]
+if pclass:
+    filtered_df = filtered_df[filtered_df["class"].isin(pclass)]
+
+if age:
+    filtered_df = filtered_df[(filtered_df["age"]>= age[0])&
+                              (filtered_df['age']<= age[1])]
+
+st.dataframe(filtered_df)
+
+fig1 = px.histogram(filtered_df,x = 'age', nbins= 30, title = "Age Distribution")
+
+st.plotly_chart(fig1,use_container_width=True)
+st.markdown('''**Conclusion**: The age distribution of titanic passenges is right-skewed,
+            with a higher concentration of younger passengers.
+            There is noticible drop in the no of passengers above 
+            60 years old''')
+
+
+#  Add bar chart for survival count. Add pie chart for passengers class In the dashboard
+
+
+
+
+
